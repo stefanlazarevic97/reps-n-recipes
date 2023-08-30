ff0e0e7785b174b3acdbb5e9198a6e21b9b2de78
ncar285 <carlier.nicholas@gmail.com>
2023-08-29 20:44:01 -0700

    light-weight placeholder for create a workout component

--- frontend/src/components/WorkoutForm/WorkoutForm.jsx
+++ frontend/src/components/WorkoutForm/WorkoutForm.jsx
@@ -7,6 +7,9 @@ import './WorkoutForm.css';
 
 const WorkoutForm = () => {
 
+    const [addExercise, setAddExercise] = useState(false)
+    const [selectedExercise, setSelectedExercise] = useState('')
+    const [exerciseList, setExerciseList] = useState([])
     const dispatch = useDispatch()
     const active = useSelector(getWorkoutFormState)
 
@@ -17,24 +20,65 @@ const WorkoutForm = () => {
         dispatch(deactivateHealthForm())
     }
 
-
     const handleSubmit = async e => {
         e.preventDefault();
     }
 
+    const selectedAnExercise = e => {
+        const exercise = e.currentTarget.value
+        setSelectedExercise(exercise)
+    }
+
+    const handleAdd = () => {
+        const newList = [...exerciseList, selectedExercise]
+        setExerciseList(newList)
+    }
+
+    const makeExerciseList = () => {
+
+        const list = exerciseList.map((exercise)=>{
+            return (
+                <li key={exercise} className='exercise-ele'>
+                    <div>{exercise}</div>
+                    <div>kg input...</div>
+                    <div>reps input...</div>
+                </li>
+            )
+        })
+        return (
+            <>
+                <ul>
+                    {list}
+                </ul>
+            </>
+        )
+    }
 
     return (
         <div className="workout-form-container">
         <div className="workout-form-background" onClick={handleExit}>
         </div>
-
             <form className="workout-form" onSubmit={handleSubmit}>
 
+                {makeExerciseList()}
+
+                <button onClick={()=>setAddExercise(true)}>Add Exercises</button>
+                {addExercise && 
+                    <div>
+                        <input type="text" placeholder='search'/>
+                        <div>
+                            <select onChange={selectedAnExercise} value={selectedExercise}>
+                                <option value="crunch">Crunch</option>
+                                <option value="bench">Bench Press</option>
+                                <option value="bulg-squat">Bulgarian Split Squats</option>
+                            </select>
+                        </div>
+                        <button onClick={handleAdd}>Add</button>
+                    </div>
+                }
             </form>  
-            
         </div>
     )
-
 }
 
 export default WorkoutForm
