9fbc0ce0d5ff5f70abc98f172909e636afd64294
Elliot Chang <elliotchang126@gmail.com>
2023-08-31 18:14:36 -0700

    setup workouts reducer

--- frontend/src/store/workout.jsx
+++ frontend/src/store/workout.jsx
@@ -1,17 +1,38 @@
 import jwtFetch from "./jwt"
 
+const RECEIVE_WORKOUTS = 'workout/RECEIVE_WORKOUTS'
 const RECEIVE_WORKOUT = 'workout/RECEIVE_WORKOUT'
+const REMOVE_WORKOUT = 'workout/REMOVE_WORKOUT'
 const RECEIVE_WORKOUT_ERRORS = 'workout/RECEIVE_WORKOUT_ERRORS'
+const CLEAR_WORKOUT_ERRORS = 'workout/CLEAR_WORKOUT_ERRORS'
+
+const receiveWorkouts = workouts => ({
+    type: RECEIVE_WORKOUTS,
+    workouts
+})
 
 const receiveWorkout = workout => ({
     type: RECEIVE_WORKOUT,
     workout
 })
 
+const removeWorkout = workoutId => ({
+    type: REMOVE_WORKOUT,
+    workoutId
+})
+
 const receiveWorkoutErrors = errors => ({
     type: RECEIVE_WORKOUT_ERRORS,
-    errorsr
+    errors
 })
+
+const clearWorkoutErrors = () => ({
+    type: CLEAR_WORKOUT_ERRORS
+})
+
+const getWorkouts = state => Object.values(state.workouts)
+
+const getWorkout = workoutId => state => state.workouts[workoutId]
 
 export const createWorkout = workout => async dispatch => {
     try {
@@ -27,4 +48,19 @@ export const createWorkout = workout => async dispatch => {
     }
 }
 
-const workoutReducer = (state = initalState)
+const workoutsReducer = (state = {}, action) => {
+    const nextState = { ...state }
+    switch (action.type) {
+        case RECEIVE_WORKOUTS:
+            return { ...action.workouts }
+        case RECEIVE_WORKOUT:
+            return { ...state, [action.workout.id]: action.workout}
+        case REMOVE_WORKOUT:
+            delete nextState[action.workoutId]
+            return nextState
+        default:
+            return state
+    }
+}
+
+export default workoutsReducer
