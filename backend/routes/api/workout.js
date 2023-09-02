6596832ada3fab8e8e4f3d229edc5b693ed5adf2
Elliot Chang <elliotchang126@gmail.com>
2023-09-01 21:48:41 -0700

    removed more commented out code

--- backend/routes/api/workout.js
+++ backend/routes/api/workout.js
@@ -45,40 +45,5 @@ router.post('/', requireUser, async (req, res, next) => {
         next(err)
     }
 })
-// router.patch('/:id', requireUser, validateExerciseInput, async (req, res, next) => {
-//     try {
-//         const { id } = req.params;
-//         const updatedExercise = await Exercise.findOneAndUpdate(
-//             { _id: id, performer: req.user._id },
-//             req.body,
-//             { new: true }
-//         ).populate('performer', '_id username');
-
-//         if (!updatedExercise) {
-//             return res.status(404).json({ error: 'Exercise not found or unauthorized' });
-//         }
-
-//         return res.json(updatedExercise);
-//     } catch (err) {
-//         next(err)
-//     }
-// })
-  
-// router.delete('/:id', requireUser, async (req, res, next) => {
-//     try {
-//         const { id } = req.params;
-//         const deletedExercise = await Exercise.findOneAndDelete(
-//             { _id: id, performer: req.user._id }
-//         ).populate('performer', '_id username');
-
-//         if (!deletedExercise) {
-//             return res.status(404).json({ error: 'Exercise not found or unauthorized' });
-//         }
-
-//         return res.json({ message: 'Exercise successfully deleted', deletedExerciseId: deletedExercise._id });
-//     } catch (err) {
-//         next(err)
-//     }
-// })
 
 module.exports = router;
